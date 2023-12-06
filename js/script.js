620cd94e24fc86e52f4702652e21b82f5b46bf00
Donato-Carola <don94@live.it>
2023-12-06 12:13:11 +0100

    prova al click sul contatto e stampare i messaggi, ma nulla

--- js/script.js
+++ js/script.js
@@ -13,21 +13,25 @@ createApp({
     return {
         contacts: [
             {
+                id:0,
                 name: 'Michele',
                 avatar: './img/avatar_1.jpg',
                 visible: true,
                 messages: [
                     {
+                        id:0,
                         date: '10/01/2020 15:30:55',
                         message: 'Hai portato a spasso il cane?',
                         status: 'sent'
                     },
                     {
+                        id:1,
                         date: '10/01/2020 15:50:00',
                         message: 'Ricordati di stendere i panni',
                         status: 'sent'
                     },
                     {
+                        id:2,
                         date: '10/01/2020 16:15:22',
                         message: 'Tutto fatto!',
                         status: 'received'
@@ -35,21 +39,25 @@ createApp({
                 ],
             },
             {
+                id:1,
                 name: 'Fabio',
                 avatar: './img/avatar_2.jpg',
                 visible: true,
                 messages: [
                     {
+                        id:3,
                         date: '20/03/2020 16:30:00',
                         message: 'Ciao come stai?',
                         status: 'sent'
                     },
                     {
+                        id:4,
                         date: '20/03/2020 16:30:55',
                         message: 'Bene grazie! Stasera ci vediamo?',
                         status: 'received'
                     },
                     {
+                        id:5,
                         date: '20/03/2020 16:35:00',
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
                         status: 'sent'
@@ -57,21 +65,25 @@ createApp({
                 ],
             },
             {
+                id:2,
                 name: 'Samuele',
                 avatar: './img/avatar_3.jpg',
                 visible: true,
                 messages: [
                     {
+                        id:6,
                         date: '28/03/2020 10:10:40',
                         message: 'La Marianna va in campagna',
                         status: 'received'
                     },
                     {
+                        id:7,
                         date: '28/03/2020 10:20:10',
                         message: 'Sicuro di non aver sbagliato chat?',
                         status: 'sent'
                     },
                     {
+                        id:8,
                         date: '28/03/2020 16:15:22',
                         message: 'Ah scusa!',
                         status: 'received'
@@ -79,16 +91,19 @@ createApp({
                 ],
             },
             {
+                id:3,
                 name: 'Alessandro B.',
                 avatar: './img/avatar_4.jpg',
                 visible: true,
                 messages: [
                     {
+                        id:9,
                         date: '10/01/2020 15:30:55',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
                         status: 'sent'
                     },
                     {
+                        id:10,
                         date: '10/01/2020 15:50:00',
                         message: 'Si, ma preferirei andare al cinema',
                         status: 'received'
@@ -96,16 +111,19 @@ createApp({
                 ],
             },
             {
+                id:4,
                 name: 'Alessandro L.',
                 avatar: './img/avatar_5.jpg',
                 visible: true,
                 messages: [
                     {
+                        id:11,
                         date: '10/01/2020 15:30:55',
                         message: 'Ricordati di chiamare la nonna',
                         status: 'sent'
                     },
                     {
+                        id:12,
                         date: '10/01/2020 15:50:00',
                         message: 'Va bene, stasera la sento',
                         status: 'received'
@@ -113,21 +131,25 @@ createApp({
                 ],
             },
             {
+                id:5,
                 name: 'Claudia',
                 avatar: './img/avatar_6.jpg',
                 visible: true,
                 messages: [
                     {
+                        id:13,
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao Claudia, hai novità?',
                         status: 'sent'
                     },
                     {
+                        id:14,
                         date: '10/01/2020 15:50:00',
                         message: 'Non ancora',
                         status: 'received'
                     },
                     {
+                        id:15,
                         date: '10/01/2020 15:51:00',
                         message: 'Nessuna nuova, buona nuova',
                         status: 'sent'
@@ -135,16 +157,19 @@ createApp({
                 ],
             },
             {
+                id:6,
                 name: 'Federico',
                 avatar: './img/avatar_7.jpg',
                 visible: true,
                 messages: [
                     {
+                        id:16,
                         date: '10/01/2020 15:30:55',
                         message: 'Fai gli auguri a Martina che è il suo compleanno!',
                         status: 'sent'
                     },
                     {
+                        id:17,
                         date: '10/01/2020 15:50:00',
                         message: 'Grazie per avermelo ricordato, le scrivo subito!',
                         status: 'received'
@@ -152,21 +177,25 @@ createApp({
                 ],
             },
             {
+                id:7,
                 name: 'Davide',
                 avatar: './img/avatar_8.jpg',
                 visible: true,
                 messages: [
                     {
+                        id:18,
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao, andiamo a mangiare la pizza stasera?',
                         status: 'received'
                     },
                     {
+                        id:19,
                         date: '10/01/2020 15:50:00',
                         message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
                         status: 'sent'
                     },
                     {
+                        id:20,
                         date: '10/01/2020 15:51:00',
                         message: 'OK!!',
                         status: 'received'
@@ -175,6 +204,11 @@ createApp({
             }
         ]
         
+    }
+  },
+  methods:{
+    onMessageClick(message){
+        message.visible = !message.visible;
     }
   }
 }).mount('#app')
